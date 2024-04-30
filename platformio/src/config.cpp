4bdfeae6fb96c170cc76ad2bc7dce0969db75f3d
don wade <don_wade@rogers.com>
2024-04-30 13:20:41 -0400

    reverting to api 3.0

--- platformio/src/config.cpp
+++ platformio/src/config.cpp
@@ -83,7 +83,7 @@ const String OWM_ENDPOINT = "api.openweathermap.org";
 // - Go to https://home.openweathermap.org/subscriptions and set the "Calls per
 //   day (no more than)" to 1,000. This ensures you will never overrun the free
 //   calls.
-const String OWM_ONECALL_VERSION = "2.5";  //"3.0";
+const String OWM_ONECALL_VERSION = "3.0";
 
 // LOCATION
 // Set your latitude and longitude.
